03ba6fef492802b298cb6fb7375fe2331ad67112
Johnny Wu <skydodle@gmail.com>
2023-07-14 17:40:13 -0700

    Converted to tailwind

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import Container from '@mui/material/Container';
-import { Typography, Box, Button } from '@mui/material';
 import { Pets } from '@mui/icons-material';
 import { useNavigate } from 'react-router-dom';
 
@@ -8,45 +6,33 @@ const Hero: React.FC = () => {
   const navigate = useNavigate();
 
   return (
-    <Box
-      sx={{
-        bgcolor: 'background.paper',
-        pt: 10,
-        pb: 6,
-        pl: 0,
-        height: '100%',
-      }}
-    >
-      <Container maxWidth='sm'>
-        <Typography
-          variant='h3'
-          align='center'
-          color='textPrimary'
-          gutterBottom
-          sx={{ fontWeight: 700 }}
-        >
-          Let's find a match!
-        </Typography>
-        <Typography
-          variant='h6'
-          align='center'
-          color='textPrimary'
-          paragraph
-          sx={{ fontWeight: 500 }}
-        >
+    <div className="bg-off-white pt-40 pb-24 pl-0 min-h-screen">
+      <div className="mx-auto p-5 ">
+        <h1 className="text-4xl text-center font-bold mb-4 font-lexend">
+          Lets find a match!
+        </h1>
+        <h2 className="text-xl text-center font-light font-lexend">
           Here at
-          <Pets />
-          FetchPups, we love our furry best friends. Our mission is to help a
-          dog-lover like yourself find a sheltered dog to join your family. Pick
-          a few of your favorite dogs and we will find you a perfect match!
-        </Typography>
-        <Box sx={{ display: 'flex', justifyContent: 'center', mt: 4 }}>
-          <Button variant='contained' onClick={() => navigate('/search')}>
+          <span className="text-custom-blue font-extrabold font-roboto">
+            {' '}
+            <Pets />
+            FetchPups
+          </span>
+          , we love our furry best friends. Our mission is to help a dog-lover
+          like yourself find a sheltered dog to join your family. Pick a few of
+          your favorite dogs and we will find you a perfect match!
+        </h2>
+
+        <div className="flex justify-center mt-6">
+          <button
+            className="bg-vibrant-orange hover:bg-orange-700 text-white font-lexend py-2 px-4 rounded"
+            onClick={() => navigate('/search')}
+          >
             Start Searching
-          </Button>
-        </Box>
-      </Container>
-    </Box>
+          </button>
+        </div>
+      </div>
+    </div>
   );
 };
 
