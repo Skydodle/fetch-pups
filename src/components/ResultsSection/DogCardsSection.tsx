28443a36ee78b092330851bdb69c507cb0d38b57
Johnny Wu <skydodle@gmail.com>
2023-07-15 16:43:23 -0700

    FIxed md col display

--- src/components/ResultsSection/DogCardsSection.tsx
+++ src/components/ResultsSection/DogCardsSection.tsx
@@ -15,7 +15,7 @@ const DogCardsSection: React.FC<DogCardsSectionProps> = ({
   favorites,
 }) => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-1">
+    <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-1">
       {favorites.length === 0 && showFavorite ? (
         <EmptyFavoritesAlert />
       ) : (
