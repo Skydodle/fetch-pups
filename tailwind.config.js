a596fb180929b017b8e4343e750778248a141438
Johnny Wu <skydodle@gmail.com>
2023-07-14 17:38:10 -0700

    Added custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-blue': '#1976D2',
+        'vibrant-orange': '#FF5722',
+        'off-white': '#F9F7F2',
+      },
+    },
+    fontFamily: {
+      IBM: ['IBM Plex Sans Condensed', 'sans-serif'],
+      syne: ['Syne', 'sans-serif'],
+      lexend: ['Lexend', 'san-serif'],
+      roboto: ['Roboto', 'san-serif'],
+    },
   },
   plugins: [],
 };
