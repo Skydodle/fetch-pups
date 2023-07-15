c6b3b69c9c479ba9fc9f378967e63510c7285c92
Johnny Wu <skydodle@gmail.com>
2023-07-15 16:44:17 -0700

    Added title margin

--- src/components/Filter/SearchBarSection.tsx
+++ src/components/Filter/SearchBarSection.tsx
@@ -5,7 +5,7 @@ const SearchBarSection: React.FC = () => {
   return (
     <>
       <div className="p-4 mt-10 bg-white flex flex-col">
-        <h2 id="filter-title" className="text-primary font-semibold mb-2">
+        <h2 id="filter-title" className="text-primary font-semibold mb-4">
           Search By Zip Code
         </h2>
         <div className="grid grid-cols-1">
