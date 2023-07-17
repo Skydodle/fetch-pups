f64564e0c6988e61255897fa62c359a0775c7b58
Johnny Wu <skydodle@gmail.com>
2023-07-17 04:37:54 -0700

    Tried animation, failed; kept for future use

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,17 +20,26 @@ module.exports = {
       height: {
         'screen-minus-nav': 'calc(100vh - 112px)',
       },
+      animation: {
+        slide: 'slide 20s linear infinite',
+      },
+      keyframes: {
+        slide: {
+          '0%': { 'background-position': '0% 0%' },
+          '100%': { 'background-position': '100% 0%' },
+        },
+      },
+      fontFamily: {
+        IBM: ['IBM Plex Sans Condensed', 'sans-serif'],
+        syne: ['Syne', 'sans-serif'],
+        lexend: ['Lexend', 'san-serif'],
+        roboto: ['Roboto', 'san-serif'],
+      },
     },
-    fontFamily: {
-      IBM: ['IBM Plex Sans Condensed', 'sans-serif'],
-      syne: ['Syne', 'sans-serif'],
-      lexend: ['Lexend', 'san-serif'],
-      roboto: ['Roboto', 'san-serif'],
-    },
-  },
-  variants: {
-    extend: {
-      position: ['responsive', 'sticky'], // enabling the position variant for responsive design
+    variants: {
+      extend: {
+        position: ['responsive', 'sticky'], // enabling the position variant for responsive design
+      },
     },
   },
   plugins: [],
