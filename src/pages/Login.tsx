cb384554f4c35c4ac44db42d84d59568c61a6446
Johnny Wu <skydodle@gmail.com>
2023-07-17 01:06:28 -0700

    Converted to tailwind.

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,79 +1,29 @@
 import React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { Pets } from '@mui/icons-material';
 import LoginForm from '../components/LoginForm';
 import useMediaQuery from '@mui/material/useMediaQuery';
 
-const defaultTheme = createTheme();
-
-const Login = () => {
+const Login: React.FC = () => {
   const matches = useMediaQuery('(max-width:600px)');
-
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Container
-        component="main"
-        maxWidth="xl"
-        sx={{
-          backgroundImage: `url(${require('../assets/bg1.jpeg')})`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          minHeight: '100vh',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'flex-start',
-          padding: '2rem',
-        }}
-      >
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            width: '100%',
-            height: '100%',
-            justifyContent: 'flex-start',
-          }}
+    <div
+      className=" min-h-screen flex flex-col items-center justify-start p-8 bg-center bg-cover"
+      style={{ backgroundImage: `url(${require('../assets/bg1.jpeg')})` }}
+    >
+      <div className="flex flex-col items-center w-full h-full justify-start">
+        <h1 className="text-center text-white mt-12 font-semibold text-3xl">
+          Welcome to FetchPups
+        </h1>
+        <div
+          className={`${
+            matches
+              ? 'flex flex-col items-center mt-60'
+              : 'self-start mt-20 pl-20'
+          }`}
         >
-          <Typography
-            variant="h3"
-            component="h1"
-            align="center"
-            color="white"
-            sx={{ marginTop: '3rem', fontWeight: 700 }}
-          >
-            Welcome to FetchPups
-          </Typography>
-
-          {matches ? (
-            <Box
-              sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                marginTop: '15rem',
-              }}
-            >
-              <LoginForm />
-            </Box>
-          ) : (
-            <Box
-              sx={{
-                alignSelf: 'flex-start',
-                marginTop: '5rem',
-                paddingLeft: '10%',
-              }}
-            >
-              <LoginForm />
-            </Box>
-          )}
-        </Box>
-      </Container>
-    </ThemeProvider>
+          <LoginForm />
+        </div>
+      </div>
+    </div>
   );
 };
 
