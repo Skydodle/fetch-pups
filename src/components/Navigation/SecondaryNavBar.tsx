550e7cbb9fdd0c031ed20ab5f4227785bfbe95ff
Johnny Wu <skydodle@gmail.com>
2023-07-16 23:59:24 -0700

    Styled buttons colors added hover

--- src/components/Navigation/SecondaryNavBar.tsx
+++ src/components/Navigation/SecondaryNavBar.tsx
@@ -4,18 +4,18 @@ import { useNavigate } from 'react-router-dom';
 const navButtons = [
   { name: 'About', path: '/about' },
   { name: 'Search', path: '/search' },
-  { name: 'Dog Care Tips', path: '/tips' },
+  { name: 'Dog Care', path: '/tips' },
 ];
 
 const SecondaryNavBar: React.FC = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="bg-white">
+    <div className="bg-white shadow-md">
       <div className="flex justify-evenly p-2">
         {navButtons.map((button) => (
           <button
-            className="text-custom-blue text-lg font-bold"
+            className={`text-custom-blue px-2 py-1 rounded-md hover:text-indigo-700 text-lg font-lexend font-medium transition-colors duration-200`}
             onClick={() => navigate(button.path)}
             key={button.name}
           >
