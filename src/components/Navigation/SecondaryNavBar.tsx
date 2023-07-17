cc5f99c3310e22057dddd47f536e0d2e0a494d28
Johnny Wu <skydodle@gmail.com>
2023-07-17 00:08:16 -0700

    Added active state color change

--- src/components/Navigation/SecondaryNavBar.tsx
+++ src/components/Navigation/SecondaryNavBar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 const navButtons = [
   { name: 'About', path: '/about' },
@@ -9,13 +9,18 @@ const navButtons = [
 
 const SecondaryNavBar: React.FC = () => {
   const navigate = useNavigate();
+  const location = useLocation();
 
   return (
     <div className="bg-white shadow-md">
       <div className="flex justify-evenly p-2">
         {navButtons.map((button) => (
           <button
-            className={`text-custom-blue px-2 py-1 rounded-md hover:text-indigo-700 text-lg font-lexend font-medium transition-colors duration-200`}
+            className={`${
+              location.pathname === button.path
+                ? 'text-indigo-700'
+                : 'text-custom-blue'
+            } px-2 py-1 rounded-md hover:text-indigo-700 text-lg font-lexend font-medium transition-colors duration-200`}
             onClick={() => navigate(button.path)}
             key={button.name}
           >
