1c2319cdc6510e87668a7479ea5374f2231a25c1
Johnny Wu <skydodle@gmail.com>
2023-07-16 17:46:56 -0700

    Added responsive grid col numbers

--- src/components/ResultsSection/DogCardsSection.tsx
+++ src/components/ResultsSection/DogCardsSection.tsx
@@ -15,7 +15,7 @@ const DogCardsSection: React.FC<DogCardsSectionProps> = ({
   favorites,
 }) => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-1">
+    <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-3">
       {favorites.length === 0 && showFavorite ? (
         <EmptyFavoritesAlert />
       ) : (
