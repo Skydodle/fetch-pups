f7a987dd3793285b1146b4ba49ef9b5f59e8c32b
Johnny Wu <skydodle@gmail.com>
2023-07-16 19:19:42 -0700

    Converted to tailwind

--- src/components/ResultsSection/EmptyFavoritesAlert.tsx
+++ src/components/ResultsSection/EmptyFavoritesAlert.tsx
@@ -1,35 +1,20 @@
 import React from 'react';
-import { Box, Container } from '@mui/material';
-import Typography from '@mui/material/Typography';
 import PetsIcon from '@mui/icons-material/Pets';
 
 const EmptyFavoritesAlert: React.FC = () => {
   return (
-    <Container sx={{ py: 5 }}>
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          mt: 3,
-          py: 10,
-        }}
-      >
-        <Typography variant='h6' color='textSecondary' align='center'>
+    <div className="py-20">
+      <div className="flex flex-col items-center mt-3 py-20">
+        <h2 className="text-center text-gray-500 text-lg">
           Oops! You haven't added any favorites yet...
-        </Typography>
-        <Typography
-          variant='subtitle1'
-          color='textSecondary'
-          align='center'
-          gutterBottom
-        >
+        </h2>
+        <p className="text-center text-gray-500 mb-5">
           Let's pick some of your favorite dogs so we can generate a match for
           ya!
-        </Typography>
-        <PetsIcon color='secondary' fontSize='large' />
-      </Box>
-    </Container>
+        </p>
+        <PetsIcon color="secondary" fontSize="large" />
+      </div>
+    </div>
   );
 };
 
