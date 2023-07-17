4033695448497907e032f66cc78dd0bf0ca72c36
Johnny Wu <skydodle@gmail.com>
2023-07-17 04:52:17 -0700

    Centered carousel

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -19,6 +19,9 @@ const Login: React.FC = () => {
         width: '100%',
         height: '100vh',
         overflow: 'hidden',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
       }}
     >
       <Carousel
