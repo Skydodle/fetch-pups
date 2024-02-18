7735bc2298fbe4dace4810c5213450928a72904f
Johnny Wu <skydodle@gmail.com>
2024-02-18 23:17:17 +0800

    Moved sorting from filter to results toolbar

--- src/components/ResultsSection/ResultsToolBar.tsx
+++ src/components/ResultsSection/ResultsToolBar.tsx
@@ -3,11 +3,15 @@ import FavoritesButton from '../common/Buttons/FavoriteButton';
 import ClearFavoritesButton from '../common/Buttons/ClearFavoritesButton';
 import MatchButton from '../common/Buttons/MatchButton';
 import SearchButton from '../common/Buttons/SearchButton';
+import { ArrowDownward, ArrowUpward } from '@mui/icons-material';
+import TuneIcon from '@mui/icons-material/Tune';
 
 type ResultsToolbarProps = {
   favoritesCount: number;
   showFavorite: boolean;
   toggleShowFavorites: () => void;
+  handleSort: () => void;
+  asc: boolean;
   totalResults: number;
 };
 
@@ -16,6 +20,8 @@ const ResultsToolbar: React.FC<ResultsToolbarProps> = ({
   showFavorite,
   toggleShowFavorites,
   totalResults,
+  handleSort,
+  asc,
 }) => {
   return (
     <div
@@ -34,10 +40,23 @@ const ResultsToolbar: React.FC<ResultsToolbarProps> = ({
           {showFavorite ? <SearchButton /> : null}
         </div>
       </div>
-      <div className=" flex items-center justify-end">
+      <div className="flex items-center justify-end">
         <p className="text-custom-blue font-semibold pr-2">
           <b>{totalResults}</b> Results
         </p>
+      </div>
+      <div className="flex items-center justify-end">
+        <button
+          onClick={handleSort}
+          className="w-full py-1 px-2 rounded border border-custom-blue text-custom-blue text-sm "
+        >
+          {asc ? (
+            <ArrowDownward fontSize="small" />
+          ) : (
+            <ArrowUpward fontSize="small" />
+          )}{' '}
+          SORT
+        </button>
       </div>
     </div>
   );
--- src/components/SearchSection/FilterSection.tsx
+++ src/components/SearchSection/FilterSection.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import { ArrowDownward, ArrowUpward } from '@mui/icons-material';
-import TuneIcon from '@mui/icons-material/Tune';
 import FilterContent from './FilterContent';
 
 interface FilterProps {
-  asc: boolean;
   selectedBreeds: string[];
   setSelectedBreeds: React.Dispatch<React.SetStateAction<string[]>>;
   ageRange: number | number[];
   ageMin: number;
   ageMax: number;
-  handleSort: () => void;
   handleAgeRangeSlider: (event: any, newValue: number | number[]) => void;
 }
 
 const FilterSection: React.FC<FilterProps> = ({
-  asc,
   selectedBreeds,
   setSelectedBreeds,
   ageRange,
-  handleSort,
   handleAgeRangeSlider,
   ageMin,
   ageMax,
@@ -27,19 +21,6 @@ const FilterSection: React.FC<FilterProps> = ({
   return (
     <div className="p-4 mt-4 bg-white flex flex-col">
       <h1 className="text-primary text-base font-bold">Search By Filter</h1>
-      {/* <div className="col-span-1 md:col-span-1 lg:col-span-1">
-        <button
-          onClick={handleSort}
-          className="w-full py-1 px-2 rounded-r-md border border-custom-blue text-custom-blue text-sm "
-        >
-          {asc ? (
-            <ArrowDownward fontSize="small" />
-          ) : (
-            <ArrowUpward fontSize="small" />
-          )}{' '}
-          SORT
-        </button>
-      </div> */}
       <FilterContent
         selectedBreeds={selectedBreeds}
         setSelectedBreeds={setSelectedBreeds}
