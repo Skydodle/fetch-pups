e930b6c926ebf3aa1e0e9e87777c5caa39b070e8
Johnny Wu <skydodle@gmail.com>
2024-02-20 00:36:35 +0800

    Added props for zipcode search and city search

--- src/components/SearchSection/LocationSection/LocationSection.tsx
+++ src/components/SearchSection/LocationSection/LocationSection.tsx
@@ -1,6 +1,31 @@
-import SearchByZipCode from './SearchByZipCode';
+import { useState } from 'react';
+import { useZipCodes } from '../../../context/ZipCodesContext';
+import APIService from '../../../services/APIService';
+import { extractUniqueZipCodes } from '../../../utils/utils';
+import SearchByCriteria from './SearchByCriteria';
 
 const LocationSection: React.FC = () => {
+  const { zipCodes, setZipCodes, removeZipCode } = useZipCodes();
+  const [cities, setCities] = useState<string[]>([]);
+
+  const handleZipCodeSearch = (zipCode: string) => {
+    setZipCodes([...zipCodes, zipCode]);
+  };
+
+  const handleCitySearch = async (city: string) => {
+    try {
+      const locationsResponse = await APIService.fetchLocationsByCityOrState({
+        city,
+      });
+      const locations = locationsResponse.data.results;
+      console.log(locations);
+      const newZipCodes = extractUniqueZipCodes(locations);
+      setZipCodes(newZipCodes); // Update the context with new zip codes
+    } catch (error) {
+      console.error('Error fetching locations:', error);
+    }
+  };
+
   return (
     <>
       <div className="p-4  bg-white flex flex-col">
@@ -10,11 +35,31 @@ const LocationSection: React.FC = () => {
         <div className="grid grid-cols-1 gap-4">
           <div>
             <h4 className="text-sm mb-3 text-custom-blue">Zip Code:</h4>
-            <SearchByZipCode />
+            {/* SearchByZipCode */}
+            <SearchByCriteria
+              placeholder="Enter zip code(s)..."
+              validateInput={(input) => /^\d{5}$/.test(input)}
+              errorMessage="Please enter a valid 5-digit zip code"
+              onSearch={handleZipCodeSearch}
+              criteriaArray={zipCodes}
+              onRemoveCriteria={removeZipCode}
+            />
           </div>
           <div>
             <h4 className="text-sm mb-3 text-custom-blue">City:</h4>
-            {/* component here */}
+            {/* SearchByCity */}
+            <SearchByCriteria
+              placeholder="Enter city..."
+              onSearch={handleCitySearch}
+              validateInput={(input) => /^[a-zA-Z\s]+$/.test(input)}
+              errorMessage="Please enter a valid city name"
+              criteriaArray={cities}
+              onRemoveCriteria={(zipCode) => {
+                // Handle removal from both zipCodes context and citiesState
+                removeZipCode(zipCode);
+                setCities(cities.filter((code) => code !== zipCode));
+              }}
+            />
           </div>
           <div>
             <h4 className="text-sm mb-3 text-custom-blue">State:</h4>
