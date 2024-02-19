f3e1adcad782d8787d02b344016456ff903f9045
Johnny Wu <skydodle@gmail.com>
2024-02-20 01:53:42 +0800

    Added criteria adding and removing handling logics

--- src/components/SearchSection/LocationSection/LocationSection.tsx
+++ src/components/SearchSection/LocationSection/LocationSection.tsx
@@ -6,24 +6,79 @@ import SearchByCriteria from './SearchByCriteria';
 
 const LocationSection: React.FC = () => {
   const { zipCodes, setZipCodes, removeZipCode } = useZipCodes();
+  const [zipCodeSearch, setZipCodeSearch] = useState<string[]>([]);
   const [cities, setCities] = useState<string[]>([]);
+  const [cityToZipMapping, setCityToZipMapping] = useState<
+    Record<string, string[]>
+  >({});
 
   const handleZipCodeSearch = (zipCode: string) => {
     setZipCodes([...zipCodes, zipCode]);
+    setZipCodeSearch((prevZipCodeSearch) => [...prevZipCodeSearch, zipCode]);
+  };
+
+  const handleRemoveZipCode = (zipCodeToRemove: string) => {
+    // Remove from zipCodeSearch for criteria display
+    setZipCodeSearch(
+      zipCodeSearch.filter((zipCode) => zipCode !== zipCodeToRemove),
+    );
+    // Remove from zipCodes context, it should no longer be on dog cards display
+    removeZipCode(zipCodeToRemove);
   };
 
   const handleCitySearch = async (city: string) => {
     try {
       const locationsResponse = await APIService.fetchLocationsByCityOrState({
-        city,
+        city: city,
+        size: 100,
       });
-      const locations = locationsResponse.data.results;
-      console.log(locations);
-      const newZipCodes = extractUniqueZipCodes(locations);
-      setZipCodes(newZipCodes); // Update the context with new zip codes
+      const filteredLocations = locationsResponse.data.results.filter(
+        (location) => location.city?.toLowerCase() === city.toLowerCase(),
+      );
+      const newZipCodes = extractUniqueZipCodes(filteredLocations);
+
+      // Update zipCodes context
+      setZipCodes(newZipCodes);
+
+      // Update city to zip code mapping
+      setCityToZipMapping((prevMapping) => {
+        const cityKey = city.toLowerCase();
+        // Assuming newZipCodes are the zip codes derived from the city search
+        const updatedZipCodes = [
+          ...(prevMapping[cityKey] || []),
+          ...newZipCodes,
+        ];
+        return { ...prevMapping, [cityKey]: updatedZipCodes };
+      });
+
+      if (!cities.includes(city)) {
+        setCities([...cities, city]);
+      }
     } catch (error) {
       console.error('Error fetching locations:', error);
     }
+  };
+
+  const handleRemoveCity = (cityToRemove: string) => {
+    const cityKey = cityToRemove.toLowerCase();
+    const zipCodesToRemove = cityToZipMapping[cityKey] || [];
+
+    // Remove these zip codes from the zipCodes context
+    setZipCodes((currentZipCodes) =>
+      currentZipCodes.filter((zipCode) => !zipCodesToRemove.includes(zipCode)),
+    );
+
+    // Remove the city from the cities state
+    setCities((currentCities) =>
+      currentCities.filter((city) => city.toLowerCase() !== cityKey),
+    );
+
+    // Update the mapping to remove the city entry
+    setCityToZipMapping((currentMapping) => {
+      const newMapping = { ...currentMapping };
+      delete newMapping[cityKey];
+      return newMapping;
+    });
   };
 
   return (
@@ -41,8 +96,8 @@ const LocationSection: React.FC = () => {
               validateInput={(input) => /^\d{5}$/.test(input)}
               errorMessage="Please enter a valid 5-digit zip code"
               onSearch={handleZipCodeSearch}
-              criteriaArray={zipCodes}
-              onRemoveCriteria={removeZipCode}
+              criteriaArray={zipCodeSearch}
+              onRemoveCriteria={handleRemoveZipCode}
             />
           </div>
           <div>
@@ -54,11 +109,7 @@ const LocationSection: React.FC = () => {
               validateInput={(input) => /^[a-zA-Z\s]+$/.test(input)}
               errorMessage="Please enter a valid city name"
               criteriaArray={cities}
-              onRemoveCriteria={(zipCode) => {
-                // Handle removal from both zipCodes context and citiesState
-                removeZipCode(zipCode);
-                setCities(cities.filter((code) => code !== zipCode));
-              }}
+              onRemoveCriteria={handleRemoveCity}
             />
           </div>
           <div>
