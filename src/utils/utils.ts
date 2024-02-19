5bb5c3142040134d3df48d629116205d8f546148
Johnny Wu <skydodle@gmail.com>
2024-02-20 00:34:07 +0800

    Added util to extract zip codes from location objects

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -13,3 +13,12 @@ export const valuetext = (value: number) => {
   return `${value}dog age`;
 };
 // "test": "react-scripts test --env=jsdom",
+
+// Utility function to extract unique zip codes from an array of objects that include a zip_code property
+export const extractUniqueZipCodes = (
+  items: Array<{ zip_code: string }>,
+): string[] => {
+  const zipCodes = items.map((item) => item.zip_code);
+  const uniqueZipCodes = [...new Set(zipCodes)];
+  return uniqueZipCodes;
+};
